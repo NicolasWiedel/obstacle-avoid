578b77ea7cb225a889d2e78a30c266c832d02707
Nicolas Wiedel <nicolas.wiedel@hotmail.de>
2018-10-11 13:13:26 +0200

    initial commit

---
+++ desktop/src/com/obstacleavoid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.obstacleavoid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.obstacleavoid.ObstacleAvoidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ObstacleAvoidGame(), config);
+	}
+}
