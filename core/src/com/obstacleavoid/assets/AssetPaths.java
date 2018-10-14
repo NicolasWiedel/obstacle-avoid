da7016684a98ac14fabdffb525ad641a89e5747f
NICOLASWIEDEL\Nicolas Wiedel <nicolaswiedel@googlemail.com>
2018-10-14 21:46:30 +0200

    HUD beginning

---
+++ core/src/com/obstacleavoid/assets/AssetPaths.java
@@ -0,0 +1,10 @@
+package com.obstacleavoid.assets;
+
+public class AssetPaths {
+
+    public static final String UI_FONT = "ui/fonts/ui_font_32.fnt";
+
+    private AssetPaths(){
+
+    }
+}
--- core/src/com/obstacleavoid/config/GameConfig.java
+++ core/src/com/obstacleavoid/config/GameConfig.java
@@ -5,6 +5,9 @@ public class GameConfig {
     public static final float WIDTH = 480f;   // pixels
     public static final float HEIGHT = 800f;  // pixels
 
+    public static final float HUD_WIDTH = 480f;  // world units
+    public static final float HUD_HEIGHT = 800f; // world units
+
     public static final float WORLD_WIDTH = 6.0f;    // world units
     public static final float WORLD_HEIGHT = 10.0f;  // world units
 
@@ -13,6 +16,7 @@ public class GameConfig {
 
     public static final float MAX_PLAYER_X_SPEED = 0.25f;
     public static final float OBSTACLE_SPAWN_TIME = 0.25f;
+    public static final int LIVES_START = 3;
 
     private GameConfig(){
 
